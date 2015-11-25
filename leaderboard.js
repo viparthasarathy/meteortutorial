14cffbeccb262a8cc86ece27dcbd96bb71e7a18c
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-11-25 16:30:39 +0900

    add helpers

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,16 @@
 PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+	Template.leaderboard.helpers({
+		'player': function(){
+			return "Some other text"
+		},
+		'otherHelperFunction': function(){
+			return "Some other function"
+		}
+	});
+}
+
+if(Meteor.isServer){
+	
+}
