60b1dca05444beb1068e476a965243528eb4fd9d
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-11-29 20:40:13 +0900

    add score function

--- leaderboard.js
+++ leaderboard.js
@@ -4,18 +4,32 @@ if(Meteor.isClient){
 	Template.leaderboard.helpers({
 		'player': function(){
 			return playersList.find();
+		},
+
+		'selectedClass': function(){
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+			if(playerId == selectedPlayer) {
+				return "selected";
+			}
+
 		}
 	});
 
 	Template.leaderboard.events({
 
 		'click .player': function(){
-			console.log("You clicked an .player element");
-			Session.set('SelectedPlayer', 'session value test');
-			var selectedPlayer = Session.get('selectedPlayer');
+			var playerId = this._id;
+			Session.set('selectedPlayer', playerId);
 
+		},
+
+		'click .increment': function(){
+			var selectedPlayer = Session.get('selectedPlayer');
+			playersList.update(selectedPlayer, {$inc: {score: 5} });
 		}
 	});
+
 }
 
 if(Meteor.isServer){
